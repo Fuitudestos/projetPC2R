0f03b7e2ac79c350e0b0d30556f6a43679667eb5
Paul Charton <paul.charton@gmail.com>
2018-05-05 15:38:49 +0200

    Correction de tirage grille

--- Serveur/src/boggle.c
+++ Serveur/src/boggle.c
@@ -4,41 +4,56 @@
 #include <stdlib.h>
 #include <time.h>
 
-const char des0[] = {'E', 'T', 'U', 'K', 'N', 'O'};
-const char des1[] = {'E', 'V', 'G', 'T', 'I', 'N'};
-const char des2[] = {'D', 'E', 'C', 'A', 'M', 'P'};
-const char des3[] = {'I', 'E', 'L', 'R', 'U', 'W'};
-const char des4[] = {'E', 'H', 'I', 'F', 'S', 'E'};
-const char des5[] = {'R', 'E', 'C', 'A', 'L', 'S'};
-const char des6[] = {'E', 'N', 'T', 'D', 'O', 'S'};
-const char des7[] = {'O', 'F', 'X', 'R', 'I', 'A'};
-const char des8[] = {'N', 'A', 'V', 'E', 'D', 'Z'};
-const char des9[] = {'E', 'I', 'O', 'A', 'T', 'A'};
-const char des10[] = {'G', 'L', 'E', 'N', 'Y', 'U'};
-const char des11[] = {'B', 'M', 'A', 'Q', 'J', 'O'};
-const char des12[] = {'T', 'L', 'I', 'B', 'R', 'A'};
-const char des13[] = {'S', 'P', 'U', 'L', 'T', 'E'};
-const char des14[] = {'A', 'I', 'M', 'S', 'O', 'R'};
-const char des15[] = {'E', 'N', 'H', 'R', 'I', 'S'};
+char des0[] = {'E', 'T', 'U', 'K', 'N', 'O'};
+char des1[] = {'E', 'V', 'G', 'T', 'I', 'N'};
+char des2[] = {'D', 'E', 'C', 'A', 'M', 'P'};
+char des3[] = {'I', 'E', 'L', 'R', 'U', 'W'};
+char des4[] = {'E', 'H', 'I', 'F', 'S', 'E'};
+char des5[] = {'R', 'E', 'C', 'A', 'L', 'S'};
+char des6[] = {'E', 'N', 'T', 'D', 'O', 'S'};
+char des7[] = {'O', 'F', 'X', 'R', 'I', 'A'};
+char des8[] = {'N', 'A', 'V', 'E', 'D', 'Z'};
+char des9[] = {'E', 'I', 'O', 'A', 'T', 'A'};
+char des10[] = {'G', 'L', 'E', 'N', 'Y', 'U'};
+char des11[] = {'B', 'M', 'A', 'Q', 'J', 'O'};
+char des12[] = {'T', 'L', 'I', 'B', 'R', 'A'};
+char des13[] = {'S', 'P', 'U', 'L', 'T', 'E'};
+char des14[] = {'A', 'I', 'M', 'S', 'O', 'R'};
+char des15[] = {'E', 'N', 'H', 'R', 'I', 'S'};
 
 char* tirageGrille(char* res)
 {
-    res[0] = des0[rand()%5];
-    res[1] = des1[rand()%5];
-    res[2] = des2[rand()%5];
-    res[3] = des3[rand()%5];
-    res[4] = des4[rand()%5];
-    res[5] = des5[rand()%5];
-    res[6] = des6[rand()%5];
-    res[7] = des7[rand()%5];
-    res[8] = des8[rand()%5];
-    res[9] = des9[rand()%5];
-    res[10] = des10[rand()%5];
-    res[11] = des11[rand()%5];
-    res[12] = des12[rand()%5];
-    res[13] = des13[rand()%5];
-    res[14] = des14[rand()%5];
-    res[15] = des15[rand()%5];
+    int i;
+    int tmp;
+    char** mesDes = malloc(sizeof(char*) * 16);
+
+    mesDes[0] = des0;
+    mesDes[1] = des1;
+    mesDes[2] = des2;
+    mesDes[3] = des3;
+    mesDes[4] = des4;
+    mesDes[5] = des5;
+    mesDes[6] = des6;
+    mesDes[7] = des7;
+    mesDes[8] = des8;
+    mesDes[9] = des9;
+    mesDes[10] = des10;
+    mesDes[11] = des11;
+    mesDes[12] = des12;
+    mesDes[13] = des13;
+    mesDes[14] = des14;
+    mesDes[15] = des15;
+
+    for(i = 0; i < 16; i++)
+    {
+        tmp = rand() % (16-i);
+
+        res[i] = mesDes[tmp][rand() % 6];
+
+        mesDes[tmp] = mesDes[15-i];
+    }
+
+    free(mesDes);
 
     return res;
 }
